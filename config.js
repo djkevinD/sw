51f7f7be48f655050647b9582c3dcb323b663713
Alfabet <77178704+djkevinD@users.noreply.github.com>
2024-08-26 12:26:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-	name: 'Wily Kun', // Ubah Nama kamu [ ini nama yg dimunculin saat terkoneksi ke WA web ]
-	owner: '6289688206739', // Ubah Nomor ke Nomor owner
+	name: 'Vin', // Ubah Nama kamu [ ini nama yg dimunculin saat terkoneksi ke WA web ]
+	owner: '6281338125255', // Ubah Nomor ke Nomor owner
 	autoread: true, // Ubah Ini untuk read sw == true, untuk tidak maka == false
 	faston: 1000, // Semakin dikit nilai nya, maka semakin cepat read SW
-    pairingNumber: "6289688206739", // nomer pairing 
+    pairingNumber: "6281338125255", // nomer pairing 
 }
 export default config;
